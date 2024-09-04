cbaa786df4b4f1e361ec893675676f40b131081e
luoweiqiang9527 <luoweiqiang9527@163.com>
2024-09-04 10:21:07 +0800

    循环获取剪切板文字并输出

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "clip/clip.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string value;
+    while (clip::has(clip::text_format())) {
+        clip::get_text(value);
+        std::cout << value << std::endl;
+    }
     return 0;
 }
